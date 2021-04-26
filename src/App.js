d7a61493cde21a4bb9794b29d40ee6f3939757e9
Jhesse-Jack <armahjames9@gmail.com>
2021-04-26 17:14:51 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import './bootstrap.min.css';
 
 function App() {
+  let [nums, setNums] = React.useState()
+  function handleInput(e){
+    setNums(e.target.value)
+    console.log(nums)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +23,72 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className="calc-container">
+        <div className="row">
+          <input 
+            type="text"
+            className="form-control"
+            onInput = {handleInput} 
+            value = {nums}
+            placeholder = "0" 
+            readOnly />
+        </div>
+        <div className="row">
+          <div className="col-3">
+            <button
+              type="button"
+              className="btn btn-light">
+              +
+            </button>
+          </div>
+          <div className="col-3">
+            <button 
+              type="button"
+              className="btn btn-light">
+                -
+              </button>
+          </div>
+          <div className="col-3">
+            <button
+              type="button"
+              className="btn btn-light">
+              &times;
+            </button>
+          </div>
+          <div className="col-3">
+            <button
+              type="button"
+              className="btn btn-light">
+              &divide;
+            </button>
+          </div>
+        </div>
+        <p></p>
+        <div className="row">
+          <div className="col-3">
+            <button
+              type="button"
+              className="btn btn-light">
+              7
+            </button>
+          </div>
+          <div className="col-3">
+            <button
+              type="button"
+              className="btn btn-light">
+              8
+            </button>
+          </div>
+          <div className="col-3">
+            <button
+              type="button"
+              className="btn btn-light">
+              9
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
