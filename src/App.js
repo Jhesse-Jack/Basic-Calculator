74b3da857a75b1fbc9cf3de6988de34d68ea4ca2
Jhesse-Jack <armahjames9@gmail.com>
2021-10-09 06:30:35 +0000

    Fix bugs and create modular components for each function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,40 @@ import React from 'react';
 import './bootstrap.min.css';
 
 function App() {
-  let [nums, setNums] = React.useState()
-  function handleInput(e){
-    setNums(e.target.value)
-    console.log(nums)
+  // let [nums, setNums] = React.useState()
+  // function handleInput(e){
+  //   setNums(e.target.value)
+  //   console.log(nums)
+  const nums = document.querySelectorAll('[data-number]')
+  const ops = document.querySelectorAll('[data-operation]')
+  const equals = document.querySelector('[data-equals]')
+  const ACButt = document.querySelector('[data-all-clear]')
+  const previousOps = document.querySelector('[data-previous-operand]')
+  const currentOps = document.querySelector('[data-current-operand]')
+  console.log(nums)
+  console.log(ops)
+  function Calculator(previousOp, currentOp){
+    previousOp = previousOps
+    currentOp = currentOps
+    clear()
   }
+
+  clear = () => {
+
+  }
+  appendNum = (nums) => {
+
+  }
+  chooseOps = (ops) => {
+
+  }
+  compute = () => {
+
+  }
+  updateDisplay = () => {
+    
+  }
+  //}
   return (
     <div className="App App-header">
       <div className="calc-container">
@@ -15,31 +44,31 @@ function App() {
           <input 
             type="text"
             className="form-control"
-            onInput = {handleInput} 
+            //onInput = {handleInput} 
             value = {nums}
             placeholder = "0" 
             readOnly />
         </div>
         <div className="row">
-          <div className="calc-button col-3">
+          <div data-operation className="calc-button col-3">
             <button
               className="btn btn-primary">
               +
             </button>
           </div>
-          <div className="calc-button col-3">
+          <div data-operation className="calc-button col-3">
             <button 
               className="btn btn-primary">
                 -
               </button>
           </div>
-          <div className="calc-button col-3">
+          <div data-operation className="calc-button col-3">
             <button
               className="btn btn-primary">
               &times;
             </button>
           </div>
-          <div className="calc-button col-3">
+          <div data-operation className="calc-button col-3">
             <button
               className="btn btn-primary">
               &divide;
@@ -48,37 +77,39 @@ function App() {
         </div>
         <p></p>
           <div className="row">
+            <div data-previous-operand></div>
+            <div data-current-operand></div>
             <div className="calc-button col-3">
               <button
-                className="btn btn-light">
+                data-number className="btn btn-light">
                 7
               </button>
               <p></p>
-              <button className="btn btn-light">
+              <button data-number className="btn btn-light">
                 4
               </button>
             </div>
             <div className="calc-button col-3">
               <button
-                className="btn btn-light">
+                data-number className="btn btn-light">
                 8
               </button>
               <p></p>
-              <button className="btn btn-light">
+              <button data-number className="btn btn-light">
                 5
               </button>
             </div>
             <div className="calc-button col-3">
               <button
-                className="btn btn-light">
+                data-number className="btn btn-light">
                 9
               </button>
               <p></p>
-              <button className="btn btn-light">
+              <button data-number className="btn btn-light">
                 6
               </button>
             </div>
-            <div className="calc-button col-3">
+            <div data-all-clear className="calc-button col-3">
               <button
                 className="btn btn-primary ACbtn">
                 AC
@@ -89,33 +120,33 @@ function App() {
         <div className="row">
           <div className="calc-button0 col-6">
             <div className="row">
-              <button className="btn btn-light oneandtwo">
+              <button data-number className="btn btn-light oneandtwo">
                 1
               </button>
               <br />
-              <button className="btn btn-light oneandtwo">
+              <button data-number className="btn btn-light oneandtwo">
                 2
               </button>
             </div>
             <p></p>
             <div className="row">
-              <button className="col-9 btn btn-light">
+              <button data-number className="col-9 btn btn-light">
                 0
               </button>
             </div>
             <p></p>
           </div>
           <div className="calc-button col-3">
-            <button className="btn btn-light">
+            <button data-number className="btn btn-light">
               3
             </button>
             <p></p>
-            <button className="btn btn-light">
+            <button data-number className="btn btn-light">
               .
             </button>
           </div>
           <div className="calc-button col-3">
-            <button className="btn btn-primary ACbtn">
+            <button data-equals className="btn btn-primary ACbtn">
               =
             </button>
           </div>
